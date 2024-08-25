3f822bf2ab25bb484909a24004d0cd676d2988f8
Vladyslav Molchan <Nksb2017@gmail.com>
2024-08-25 14:22:47 +0300

    add class Task

---
+++ src/main/java/com/todoList/Task.java
@@ -0,0 +1,49 @@
+package com.todoList;
+
+
+import java.io.Serializable;
+
+public class Task implements Serializable {
+    private String title;
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void markAsCompleted() {
+        this.isCompleted = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", isCompleted=" + isCompleted +
+                '}';
+    }
+}
